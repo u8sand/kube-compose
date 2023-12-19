a2898a5424832b97f532a7e483032620e62e3197
Daniel Clarke <u8sand@gmail.com>
2023-12-19 15:44:49 -0500

    Add `kube-compose config`

---
+++ kube_compose/cli/config.py
@@ -0,0 +1,14 @@
+from kube_compose.cli import cli
+from kube_compose import utils
+
+@cli.command()
+@utils.require_binaries(helm='helm')
+@utils.require_kube_compose_release
+def config(*, helm, name, namespace, **_):
+  ''' Like `docker-compose config` but shows the kubernetes deployed config
+  '''
+  utils.run([
+    helm, 'get', 'values',
+    *(('-n', namespace,) if namespace else tuple()),
+    name,
+  ])
