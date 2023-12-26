e3b74c3f3eef8619fe8d83d7ccd7bae63f8afc73
Daniel Clarke <u8sand@gmail.com>
2023-12-26 17:02:45 -0500

    [restart]: Restart all services if one is not specified

--- kube_compose/cli/restart.py
+++ kube_compose/cli/restart.py
@@ -5,17 +5,18 @@ from kube_compose import utils
 @cli.command()
 @utils.require_binaries(kubectl='kubectl')
 @utils.require_kube_compose_release
-@click.argument('service', type=str)
-def restart(service, *, namespace, kubectl, **_):
+@click.argument('service', type=str, required=False)
+def restart(service, *, namespace, kubectl, docker_compose_config, **_):
   ''' Like `docker-compose restart` but effects the kubernetes deployed resources
   '''
+  services = [service] if service is not None else docker_compose_config.get('services', {}).keys()
   utils.run([
     kubectl, 'rollout', 'restart',
     *(('-n', namespace) if namespace else tuple()),
-    f"deploy/{service}",
+    *[f"deploy/{service}" for service in services],
   ])
   utils.run([
     kubectl, 'rollout', 'status',
     *(('-n', namespace) if namespace else tuple()),
-    f"deploy/{service}",
+    *[f"deploy/{service}" for service in services],
   ])
