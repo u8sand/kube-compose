ee9190748dc065bdf027555710d9859f52d0fcdf
Daniel J. B. Clarke <u8sand@gmail.com>
2025-06-06 12:33:58 -0400

    Add script to version/rebuild docker

---
+++ version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NEWVERSION="$(poetry version -s $@)"
+
+git add pyproject.toml || exit 1
+git commit -m ${NEWVERSION} || exit 1
+git tag v${NEWVERSION} || exit 1
+
+docker build -t u8sand/kube-compose:${NEWVERSION} . || exit 1
+docker tag u8sand/kube-compose:${NEWVERSION} u8sand/kube-compose:latest || exit 1
+docker push u8sand/kube-compose:${NEWVERSION} || exit 1
+docker push u8sand/kube-compose:latest || exit 1
