e01c6c1ae691de9a451cbd3306a5daa210521a30
Daniel Clarke <u8sand@gmail.com>
2025-06-18 13:20:19 -0400

    Fixup drift requiring namespace

--- kube_compose/cli/drift.py
+++ kube_compose/cli/drift.py
@@ -4,6 +4,7 @@ from kube_compose import utils
 
 @cli.command()
 @utils.require_binaries(kubectl='kubectl')
+@utils.require_kube_compose_release
 def drift(*, kubectl, namespace, **_):
   ''' Compare the expected next deployment with the current deployment in kubernetes
   '''
