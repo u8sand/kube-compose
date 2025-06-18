71e4e02f7973bd4fa4a69a67435256ad8ea9e527
Daniel Clarke <u8sand@gmail.com>
2025-06-18 10:02:54 -0400

    Skip volume apply if there are no volumes

--- kube_compose/cli/volume/create.py
+++ kube_compose/cli/volume/create.py
@@ -30,15 +30,17 @@ def create_volume_spec(*, volume, docker_compose_config, **_):
 @utils.require_binaries(kubectl='kubectl')
 @utils.require_kube_compose_release
 def create(*, volume, docker_compose_config, namespace, kubectl, **_):
+  volume_specs = create_volume_spec(
+    volume=volume,
+    docker_compose_config=docker_compose_config,
+    kubectl=kubectl,
+  )
+  if not volume_specs: return
   utils.run([
     *kubectl, 'apply',
     *(('-n', namespace) if namespace else tuple()),
     '-f', '-',
-  ], input=yaml.dump_all(create_volume_spec(
-    volume=volume,
-    docker_compose_config=docker_compose_config,
-    kubectl=kubectl,
-  )).encode())
+  ], input=yaml.dump_all(volume_specs).encode())
 
 @volume.command('create')
 @click.argument('volume', type=str, required=False)
@@ -50,6 +52,12 @@ def _(**kwargs):
 @utils.require_binaries(kubectl='kubectl')
 @utils.require_kube_compose_release
 def template(*, volume, docker_compose_config, namespace, kubectl, **_):
+  volume_specs = create_volume_spec(
+    volume=volume,
+    docker_compose_config=docker_compose_config,
+    kubectl=kubectl,
+  )
+  if not volume_specs: return
   # use kubectl to fill-in defaults/validate
   templ = yaml.safe_load(utils.check_output([
     *kubectl, 'apply',
