ccb7e6cb37fd289eeadf4d009e867b030bae84dd
Daniel Clarke <u8sand@gmail.com>
2025-06-18 13:17:40 -0400

    Use namespace with drift

--- kube_compose/cli/drift.py
+++ kube_compose/cli/drift.py
@@ -4,10 +4,10 @@ from kube_compose import utils
 
 @cli.command()
 @utils.require_binaries(kubectl='kubectl')
-def drift(*, kubectl, **_):
+def drift(*, kubectl, namespace, **_):
   ''' Compare the expected next deployment with the current deployment in kubernetes
   '''
   from kube_compose.cli.template import template
   utils.run([
-    *kubectl, 'diff', '-f', '-'
+    *kubectl, 'diff', *(('-n', namespace) if namespace else tuple()), '-f', '-'
   ], input=yaml.dump_all(template()).encode())
