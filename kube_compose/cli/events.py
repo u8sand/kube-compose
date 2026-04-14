1ddeb663324fcca36856ec046ce28313bd22f223
Daniel J. B. Clarke <u8sand@gmail.com>
2026-04-14 15:48:25 -0400

    Monitor events in the namespace when no service is specified

--- kube_compose/cli/events.py
+++ kube_compose/cli/events.py
@@ -5,7 +5,7 @@ from kube_compose import utils
 @cli.command()
 @utils.require_binaries(kubectl='kubectl')
 @utils.require_kube_compose_release
-@click.argument('service', type=str, required=True)
+@click.argument('service', type=str, default=None)
 def events(service, *, context, namespace, kubectl, deployments, **_):
   ''' Like `docker-compose events` but show events of the kubernetes deployed resource
   '''
@@ -15,6 +15,6 @@ def events(service, *, context, namespace, kubectl, deployments, **_):
       *(['--context', context] if context else []),
       *(['-n', namespace] if namespace else []),
       'events', 
-      '--for', deployments[service],
+      *(['--for', deployments[service]] if service else []),
       '--watch',
     ])
